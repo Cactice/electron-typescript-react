95e494b91315d37b6ed6efb793605c352f187fe8
Cactice <14835424+Cactice@users.noreply.github.com>
2020-05-31 20:16:07 +0900

    Configure react hooks

--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,16 +1,35 @@
 /**
  * React renderer.
  */
+import '@public/style.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+// import React, { useState } from 'react';
 
 // Import the styles here to process them with webpack
-import '@public/style.css';
+
 
 ReactDOM.render(
-  <div className='app'>
-    <h4>Welcome to React, Electron and Typescript</h4>
-    <p>Hello</p>
-  </div>,
-  document.getElementById('app')
+  <Example />,
+  document.getElementById('app'),
 );
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div className="app">
+      <p>
+        You clicked
+        {' '}
+        {count}
+        {' '}
+        times
+      </p>
+      <button type="button" onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
